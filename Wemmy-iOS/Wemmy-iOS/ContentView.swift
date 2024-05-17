c293c9858f651042bd7788d087662b7159a8419d
leeyebeen-dev <lyb0201190@gmail.com>
2024-05-17 09:06:19 +0900

    [Setting/#1] 프로젝트 생성

---
+++ Wemmy-iOS/Wemmy-iOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Wemmy-iOS
+//
+//  Created by 이예빈 on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wemmy-iOS/Wemmy-iOS/Wemmy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wemmy_iOSApp.swift
+//  Wemmy-iOS
+//
+//  Created by 이예빈 on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Wemmy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
