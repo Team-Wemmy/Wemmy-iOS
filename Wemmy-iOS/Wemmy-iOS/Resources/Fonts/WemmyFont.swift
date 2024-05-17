817d926fcd1cba481fb9f3d9220267d386a553bb
leeyebeen-dev <lyb0201190@gmail.com>
2024-05-17 09:21:06 +0900

    [Setting/#1] Font 추가 및 Font Extension 생성

---
+++ Wemmy-iOS/Wemmy-iOS/Resources/Fonts/WemmyFont.swift
@@ -0,0 +1,35 @@
+//
+//  WemmyFont.swift
+//  Wemmy-iOS
+//
+//  Created by 이예빈 on 5/17/24.
+//
+
+import SwiftUI
+
+extension Font {
+    // Bold
+    static let Bold36: Font = .custom("Pretendard-Bold", size: 36)
+    static let Bold30: Font = .custom("Pretendard-Bold", size: 30)
+    static let Bold24: Font = .custom("Pretendard-Bold", size: 24)
+    static let Bold20: Font = .custom("Pretendard-Bold", size: 20)
+    static let Bold14: Font = .custom("Pretendard-Bold", size: 14)
+    
+    // SemiBold
+    static let SemiBold20: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let SemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let SemiBold14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let SemiBold12: Font = .custom("Pretendard-SemiBold", size: 12)
+        
+    // Medium
+    static let Medium20: Font = .custom("Pretendard-Medium", size: 20)
+    static let Medium18: Font = .custom("Pretendard-Medium", size: 18)
+    static let Medium14: Font = .custom("Pretendard-Medium", size: 14)
+    static let Medium12: Font = .custom("Pretendard-Medium", size: 12)
+        
+    // Regular
+    static let Regular18: Font = .custom("Pretendard-Regular", size: 18)
+    static let Regular16: Font = .custom("Pretendard-Regular", size: 16)
+    static let Regular14: Font = .custom("Pretendard-Regular", size: 14)
+    static let Regular12: Font = .custom("Pretendard-Regular", size: 12)
+}
