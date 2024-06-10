15b8651788e5df7d4f5a015c4005bfb06690aab6
leeyebeen-dev <lyb0201190@gmail.com>
2024-06-10 09:17:27 +0900

    [UI/#16] 복지시설 관련 에셋 추가

--- Wemmy-iOS/Wemmy-iOS/Feature/Main/Facility/FacilityView.swift
+++ Wemmy-iOS/Wemmy-iOS/Feature/Main/Facility/FacilityView.swift
@@ -7,9 +7,18 @@
 
 import SwiftUI
 
+import MapKit
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static let GocheokCommunityServiceCenter = CLLocationCoordinate2D(latitude: 37.5004, longitude: 126.8628)
+}
+
 struct FacilityView: View {
     var body: some View {
-        Text("복지시설")
+        ZStack {
+            
+        }
     }
 }
 
